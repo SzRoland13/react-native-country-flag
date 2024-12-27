60920209d4917b3404b93daf59086eb6093b92fc
Szabo Roland <szabo.roland.1213@gmail.com>
2024-12-27 11:32:46 +0100

    This PR adds support for offline data through a separate package: - Created a separate `react-native-country-flag/data` package for offline flag assets.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Image } from "react-native";
-// import * as flag from "./flags/flagsIndex";
 
 interface CountryFlagProps {
   isoCode: string;
@@ -13,29 +12,27 @@ const FLAG_CDN_URL = "https://flagcdn.com/w80/";
 const FORMAT = ".png";
 
 const CountryFlag = ({ isoCode, size, style, flags }: CountryFlagProps) => {
+  const getFlagSource = () => {
+    if (flags) {
+      if (isoCode.toLowerCase() === "in") {
+        return flags["ind"];
+      } else if (isoCode.toLowerCase() === "do") {
+        return flags["dom"];
+      } else {
+        return flags?.[isoCode.toLowerCase()];
+      }
+    } else {
+      return { uri: FLAG_CDN_URL + isoCode.toLocaleLowerCase() + FORMAT };
+    }
+  };
+
   return (
     <Image
-      source={{ uri: FLAG_CDN_URL + isoCode.toLocaleLowerCase() + FORMAT }}
+      source={getFlagSource()}
       style={[{ width: size * 1.6, height: size }, style]}
       resizeMode="contain"
     />
   );
-
-  // TODO: Still in progress (probely available in v2.1)
-  // This switch case is just there because you can't name variables "in" and "do"
-  // switch (isoCode.toLowerCase()) {
-  //   case "in":
-  //     return <Image source={flag["ind"]} style={[{ width: size * 1.6, height: size }, style]} />;
-  //     break;
-  //   case "do":
-  //     return <Image source={flag["dom"]} style={[{ width: size * 1.6, height: size }, style]} />;
-  //     break;
-  //   default:
-  //     return (
-  //       <Image source={(flag as any)[isoCode.toLowerCase()]} style={[{ width: size * 1.6, height: size }, style]} />
-  //     );
-  //     break;
-  // }
 };
 
 export default CountryFlag;
